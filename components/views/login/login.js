6555edfc78161b66b370824ba59f57eb2ef44af2
dusgab <dustingassmann@gmail.com>
2018-08-01 17:30:37 -0300

    login con facebook

--- components/views/login/login.js
+++ components/views/login/login.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Text, View, TouchableNativeFeedback, StyleSheet, Dimensions } from 'react-native';
+import { Text, View, TouchableNativeFeedback, StyleSheet, Dimensions, Alert, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import IconM from 'react-native-vector-icons/MaterialCommunityIcons';
 import { NavigationActions } from 'react-navigation';
 
 export default class Login extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {userInfo: null};
+  }
 static navigationOptions = {left: null}
 resetTo() {
   const actionToDispatch = NavigationActions.reset({
@@ -14,6 +19,66 @@ resetTo() {
   });
   navigation.dispatch(actionToDispatch);
 }
+
+async loginFB() {
+  const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('1755097877936704', {
+      permissions: ['public_profile'],
+    });
+  if (type === 'success') {
+    // Get the user's name using Facebook's Graph API
+    const response = await fetch(
+      `https://graph.facebook.com/me?access_token=${token}&fields=id,name,picture.type(large)`);
+      const userInfo = await response.json();
+      this.setState({ userInfo });    
+  }
+}
+
+_renderUserInfo = () => {
+  return (
+    <View style={styles.bottom}>
+      <Image 
+        source={{ uri: this.state.userInfo.picture.data.url}}
+        style={{ width: 100, height: 100, borderRadius: 50, alignSelf: 'center' }} 
+      />
+      <Text style={{ fontSize: 20, textAlign: 'center', marginBottom: 10 }}> {this.state.userInfo.name} </Text>
+      <View style={styles.button}>
+      <TouchableNativeFeedback
+        onPress={() => {
+          this.props.navigation.navigate('Inicio');
+        }} style={styles.btnClickContain}>
+        <View
+          style={styles.btnContainer}>
+          <Text style={styles.btnText}>Ingresar</Text>
+        </View>
+      </TouchableNativeFeedback>
+      </View>
+    </View>
+  );
+};
+
+_renderButtonFacebook = () => {
+  return (
+    <View style={styles.bottom}>
+      <View style={styles.button}>
+      <TouchableNativeFeedback
+        onPress={() => {
+          //this.props.navigation.navigate('Inicio');
+          //this.resetTo();
+          this.loginFB();
+        }} style={styles.btnClickContain}>
+        <View
+          style={styles.btnContainer}>
+          <Icon
+            name='facebook'
+            size={30}
+            color='#fff'/>
+          <Text style={styles.btnText}>Iniciar Sesión con Facebook</Text>
+        </View>
+      </TouchableNativeFeedback>
+      </View>
+    </View>
+  );
+}
     render() {
         return (
           <View style={styles.container}>
@@ -21,24 +86,7 @@ resetTo() {
               <Text style={styles.titulo}>Karaoke Chamamecero</Text>
               <IconM name='microphone-variant' size={70}/>
             </View>
-            <View style={styles.bottom}>
-              <View style={styles.button}>
-              <TouchableNativeFeedback
-                onPress={() => {
-                  this.props.navigation.navigate('Inicio');
-                  //this.resetTo();
-                }} style={styles.btnClickContain}>
-                <View
-                  style={styles.btnContainer}>
-                  <Icon
-                    name='facebook'
-                    size={30}
-                    color='#fff'/>
-                  <Text style={styles.btnText}>Iniciar Sesión con Facebook</Text>
-                </View>
-              </TouchableNativeFeedback>
-              </View>
-            </View>
+            {!this.state.userInfo ? (this._renderButtonFacebook()) : (this._renderUserInfo())}
           </View>
         );
       }
@@ -53,7 +101,7 @@ resetTo() {
         justifyContent: 'center',
       },
       top: {
-        flex: 5,
+        flex: 3,
         alignItems: 'center',
       },
       bottom: {
